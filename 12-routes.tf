c22b9980ad5611febed55e7dc761a69ca94ef4fe
stravze <stravze@gmail.com>
2019-06-10 11:17:04 +0100

    adding azure udr routes

---
+++ 12-routes.tf
@@ -0,0 +1,14 @@
+resource "azurerm_route_table" "route_main" {
+  name                                      = "${var.vm_name}-udr"
+  location                                  = "${data.azurerm_resource_group.rg.location}"
+  resource_group_name                       = "${data.azurerm_resource_group.rg.name}"
+  disable_bgp_route_propagation             = false
+
+  route {
+    name           = "internet_out"
+    address_prefix = "0.0.0.0/0"
+    next_hop_type  = "Internets"
+  }
+
+
+}
