1e54aa360885635af13b5107f92479d95e492c4f
stravze <stravze@gmail.com>
2019-06-19 10:52:52 +0100

    adding palo ip lookup

--- 12-hub.tf
+++ 12-hub.tf
@@ -3,7 +3,11 @@ data "azurerm_resource_group" "hub" {
   name                                      = "hub-firewall"
 }
 
-data "azurerm_network_interface" "test" {
+data "azurerm_network_interface" "palo_ip" {
   name                                      = "nic-dmz-firewall-transit-0"
   resource_group_name                       = "${data.azurerm_resource_group.hub.name}"
+}
+
+locals {
+  palo_ip                         = "${data.azurerm_network_interface.palo_ip.private_ip_address}"
 }
