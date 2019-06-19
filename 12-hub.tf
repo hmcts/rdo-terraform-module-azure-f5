09258915e0ba8cfa06a72181f037166d2b71b6df
stravze <stravze@gmail.com>
2019-06-19 10:19:12 +0100

    adding hub nic

--- 12-hub.tf
+++ 12-hub.tf
@@ -1,5 +1,9 @@
 
 data "azurerm_resource_group" "hub" {
-  name                                    = "hub-firewall"
+  name                                      = "hub-firewall"
 }
 
+data "azurerm_network_interface" "test" {
+  name                                      = "nic-dmz-firewall-transit-0"
+  resource_group_name                       = "${data.azurerm_resource_group.hub.name}"
+}
