48c165e721e31dd30cfaeccb7b3fd75d11e3798a
stravze <stravze@gmail.com>
2019-07-10 13:57:39 +0100

    fixing palo lookup

--- 03-interpolated-defaults.tf
+++ 03-interpolated-defaults.tf
@@ -12,3 +12,16 @@ data "azurerm_subnet" "subnet" {
   virtual_network_name                      = "${data.azurerm_virtual_network.vnet.name}"
   resource_group_name                       = "${data.azurerm_resource_group.rg.name}"
 }
+
+data "azurerm_resource_group" "hub" {
+  name                                      = "hub-firewall"
+}
+
+data "azurerm_network_interface" "palo_ip" {
+  name                                      = "fw-sbox-nic-transit-public-ip-0"
+  resource_group_name                       = "${data.azurerm_resource_group.hub.name}"
+}
+
+locals {
+  palo_ip                                   = "${data.azurerm_network_interface.palo_ip.private_ip_address}"
+}
