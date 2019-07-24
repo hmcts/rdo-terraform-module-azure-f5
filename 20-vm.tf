2d88cf1d8a75c7f08f0d551d5d8eb2f28f1cf80a
stravze <stravze@gmail.com>
2019-07-24 10:06:20 +0100

    more testing

--- 20-vm.tf
+++ 20-vm.tf
@@ -77,7 +77,7 @@ resource "null_resource" "ansible-runs" {
 
   provisioner "local-exec" {
     command = <<EOF
-      git clone --progress --verbose https://github.com/hmcts/rdo-terraform-module-azure-f5.git
+      git clone https://github.com/hmcts/rdo-terraform-module-azure-f5.git;
       echo "ls ansible dir"
       ls -al ${path.module}/ansible
       echo "find inventory"
