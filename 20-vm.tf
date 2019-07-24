4e573079831b0adc9a5c39b055fec443869f2e54
gunnertwin <daniel@alvestechnology.io>
2019-07-24 11:56:55 +0100

    change vm version to latest

--- 20-vm.tf
+++ 20-vm.tf
@@ -14,7 +14,7 @@ resource "azurerm_virtual_machine" "vm" {
     publisher                           = "f5-networks"
     offer                               = "f5-big-ip-best"
     sku                                 = "${var.vm_sku}"
-    version                             = "14.003000" #"latest" - lastest 14.0.003xxx - takes too long to provision
+    version                             = "latest"# - lastest 14.0.003xxx - takes too long to provision
   }
   plan {
     publisher                           = "f5-networks"
