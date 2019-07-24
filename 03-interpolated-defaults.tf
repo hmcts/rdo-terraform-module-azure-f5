f162d686ff0216e9c4483655fe711a0ad1cd4bf2
gunnertwin <daniel@alvestechnology.io>
2019-07-24 11:03:55 +0100

    comment out azurerm_subnet

--- 03-interpolated-defaults.tf
+++ 03-interpolated-defaults.tf
@@ -2,11 +2,11 @@ locals {
   default_gateway                                   = "${cidrhost(data.azurerm_subnet.subnet.address_prefix,1)}"
 }
 
-data "azurerm_subnet" "subnet" {
-  name                                      = "dmz-loadbalancer"
-  virtual_network_name                      = "${data.azurerm_virtual_network.vnet.name}"
-  resource_group_name                       = "${data.azurerm_resource_group.rg.name}"
-}
+#data "azurerm_subnet" "subnet" {
+#  name                                      = "dmz-loadbalancer"
+#  virtual_network_name                      = "${data.azurerm_virtual_network.vnet.name}"
+#  resource_group_name                       = "${data.azurerm_resource_group.rg.name}"
+#}
 
 #data "azurerm_resource_group" "ctsc-email-pan-sandbox" {
 #  name                                      = "ctsc-email-pan-sandbox"
