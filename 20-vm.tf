853ea761e101fdbc021b619a3d39bf8a3596c385
stravze <stravze@gmail.com>
2019-07-25 11:38:23 +0100

    asm module

--- 20-vm.tf
+++ 20-vm.tf
@@ -78,7 +78,9 @@ resource "null_resource" "ansible-runs" {
   provisioner "local-exec" {
     command = <<EOF
       git clone https://github.com/hmcts/rdo-terraform-module-azure-f5.git;
+      cd rdo-terraform-module-azure-f5
       git clone https://github.com/hmcts/f5-asm-policy-templates.git;
+      cd ..
       #echo "ls ansible dir"
       #ls -al ${path.module}/ansible
       #echo "find inventory"
