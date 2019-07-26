1431fc1bb729978013f5c5806d46a2b6fd201dc6
stravze <stravze@gmail.com>
2019-07-26 10:56:13 +0100

    adding depends _on

--- 03-interpolated-defaults.tf
+++ 03-interpolated-defaults.tf
@@ -11,5 +11,6 @@ data "azurerm_subnet" "subnet" {
   name                                      = "dmz-loadbalancer"
   virtual_network_name                      = "${data.azurerm_virtual_network.vnet.name}"
   resource_group_name                       = "${data.azurerm_resource_group.rg.name}"
+  depends_on                                = ["data.azurerm_virtual_network.vnet"]
 }
 
