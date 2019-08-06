38384fda392dc760188911e9b88207d4ba13c532
gunnertwin <daniel@alvestechnology.io>
2019-08-06 18:26:17 +0100

    uncomment out elb

--- 15-routes.tf
+++ 15-routes.tf
@@ -1,21 +1,21 @@
-#resource "azurerm_route_table" "route_main" {
-#  name                                      = "${var.vm_name}-${var.environment}-udr"
-#  location                                  = "${data.azurerm_resource_group.rg.location}"
-##  resource_group_name                       = "${data.azurerm_resource_group.rg.name}"
- # disable_bgp_route_propagation             = true
- # tags                                      = "${var.tags}"
+resource "azurerm_route_table" "route_main" {
+  name                                      = "${var.vm_name}-${var.environment}-udr"
+  location                                  = "${data.azurerm_resource_group.rg.location}"
+  resource_group_name                       = "${data.azurerm_resource_group.rg.name}"
+  disable_bgp_route_propagation             = true
+  tags                                      = "${var.tags}"
 
 
- # route {
- #   name                                    = "to_hub_fw"
- #   address_prefix                          = "0.0.0.0/0"
- #   next_hop_type                           = "VirtualAppliance"
- #   next_hop_in_ip_address                  = "${local.palo_lb_ip}"
- # }
-#}
+  route {
+    name                                    = "to_hub_fw"
+    address_prefix                          = "0.0.0.0/0"
+    next_hop_type                           = "VirtualAppliance"
+    next_hop_in_ip_address                  = "${local.palo_lb_ip}"
+  }
+}
 
-#resource "azurerm_subnet_route_table_association" "route_association" {
-#  subnet_id                                 = "${data.azurerm_subnet.subnet.id}"
-#  route_table_id                            = "${azurerm_route_table.route_main.id}"
-#}
+resource "azurerm_subnet_route_table_association" "route_association" {
+  subnet_id                                 = "${data.azurerm_subnet.subnet.id}"
+  route_table_id                            = "${azurerm_route_table.route_main.id}"
+}
 
