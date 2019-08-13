9a850c42b16fcf8f126da9cd3f72410716d7df45
gunnertwin <daniel@alvestechnology.io>
2019-08-13 15:18:19 +0100

    change UDR route

--- 15-routes.tf
+++ 15-routes.tf
@@ -8,7 +8,7 @@ resource "azurerm_route_table" "route_main" {
 
   route {
     name                                    = "to_hub_fw"
-    address_prefix                          = "10.231.0.0/16" #need to mask
+    address_prefix                          = "10.0.0.0/8" #need to mask
     next_hop_type                           = "VirtualAppliance"
     next_hop_in_ip_address                  = "${local.palo_lb_ip}"
   }
