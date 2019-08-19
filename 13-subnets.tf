ed37fe0444d61eabb62fd75541938b1770dbd1db
gunnertwin <daniel@alvestechnology.io>
2019-08-19 11:50:27 +0100

    ignore subnet route changes

--- 13-subnets.tf
+++ 13-subnets.tf
@@ -2,7 +2,4 @@ data "azurerm_subnet" "subnet" {
   name                                    = "${var.loadbalancer_data_subnet}"
   resource_group_name                     = "${data.azurerm_resource_group.rg.name}"
   virtual_network_name                    = "${data.azurerm_virtual_network.vnet.name}"
-  lifecycle { 
-     ignore_changes                 = ["route_table_id"]
- }
 }
