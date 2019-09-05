2924455cf123c94f9700660eba22880019ee6c90
stravze <stravze@gmail.com>
2019-09-05 15:44:39 +0100

    fixing naming issues

--- 03-interpolated-defaults.tf
+++ 03-interpolated-defaults.tf
@@ -3,7 +3,7 @@ locals {
 }
 
 data "azurerm_virtual_network" "vnet" {
-  name                                      = "${data.azurerm_resource_group.rg.name}-${var.environment}"
+  name                                      = "${data.azurerm_resource_group.rg.name}"
   resource_group_name                       = "${data.azurerm_resource_group.rg.name}"
 }
 
