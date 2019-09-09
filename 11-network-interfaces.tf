a2087f635484bccbebfd9ea3b93e20902895de96
stravze <stravze@gmail.com>
2019-09-09 13:29:26 +0100

    sku

--- 11-network-interfaces.tf
+++ 11-network-interfaces.tf
@@ -17,6 +17,7 @@ resource "azurerm_public_ip" "pip_mgmt" {
   location                                  = "${data.azurerm_resource_group.rg.location}"
   resource_group_name                       = "${data.azurerm_resource_group.rg.name}"
   allocation_method                         = "Static"
+  sku                                       = "Standard"	
   tags                                      = "${var.tags}"
 }
 
