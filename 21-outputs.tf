e34daaed1e1017751185f5cb98b2eb9d61e32244
Jerry Steele <jerry.steele@hmcts.net>
2019-09-18 19:55:25 +0100

    Add outputs for f5_vnet

--- 21-outputs.tf
+++ 21-outputs.tf
@@ -1,3 +1,11 @@
 output "lb_public_ip" {
   value = "${azurerm_public_ip.pip_lb.ip_address}"
 }
+
+output "f5_vnet_id" {
+  value = "${azurerm_virtual_network.f5_vnet.id}"
+}
+
+output "f5_vnet_name" {
+  value = "${azurerm_virtual_network.f5_vnet.name}"
+}
