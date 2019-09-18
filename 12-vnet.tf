6865d067b1fdf40abf345c921748ee838a722e61
Jerry Steele <jerry.steele@hmcts.net>
2019-09-18 18:10:03 +0100

    Naughty '.'

--- 12-vnet.tf
+++ 12-vnet.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network" "f5-vnet" {
   name                                    = "${var.loadbalancer_vnet}"
   resource_group_name                     = "${data.azurerm_resource_group.rg.name}"
-  address_space                           = ["${data.azurerm_key_vault_secret.f5_vnet_cidr.value.}"]
+  address_space                           = ["${data.azurerm_key_vault_secret.f5_vnet_cidr.value}"]
   location                                = "${var.location}"
 }
