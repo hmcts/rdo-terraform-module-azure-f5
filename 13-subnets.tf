113131d5e1bf3d3a3446d6c36f9053e6c7e42552
Jerry Steele <jerry.steele@hmcts.net>
2019-09-18 09:10:01 +0100

    Take out subnet data source

--- 13-subnets.tf
+++ 13-subnets.tf
@@ -1,5 +1,5 @@
-data "azurerm_subnet" "subnet" {
-  name                                    = "${var.loadbalancer_data_subnet}"
-  resource_group_name                     = "${data.azurerm_resource_group.rg.name}"
-  virtual_network_name                    = "${data.azurerm_virtual_network.vnet.name}"
-}
+#data "azurerm_subnet" "subnet" {
+#  name                                    = "${var.loadbalancer_data_subnet}"
+#  resource_group_name                     = "${data.azurerm_resource_group.rg.name}"
+#  virtual_network_name                    = "${data.azurerm_virtual_network.vnet.name}"
+#}
