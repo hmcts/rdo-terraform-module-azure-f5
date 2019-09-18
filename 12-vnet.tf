b07f7f7078b69aa9bfa83f0941aa32694dc353ef
Jerry Steele <jerry.steele@hmcts.net>
2019-09-18 17:46:48 +0100

    Add azurerm_virtual_network resource (not data)

--- 12-vnet.tf
+++ 12-vnet.tf
@@ -1,4 +1,6 @@
-data "azurerm_virtual_network" "vnet" {
+resource "azurerm_virtual_network" "f5-vnet" {
   name                                    = "${var.loadbalancer_vnet}"
   resource_group_name                     = "${data.azurerm_resource_group.rg.name}"
+  address_space                           = ["${data.azurerm_key_vault_secret.f5_vnet_cidr.value.}"]
+  location                                = "${var.location}"
 }
