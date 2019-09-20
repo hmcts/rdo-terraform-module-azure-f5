73bc9617f6ba40d94af00c4dc545dab21436d2ad
Jerry Steele <jerry.steele@hmcts.net>
2019-09-20 15:50:40 +0100

    Copy certs from ~rdo-admin

--- 20-vm.tf
+++ 20-vm.tf
@@ -104,8 +104,7 @@ resource "null_resource" "ansible-runs" {
       echo "ansible version"
       ansible --version
       git clone https://github.com/f5devcentral/f5-asm-policy-templates.git;
-      az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID
-      az storage blob download-batch -d ${path.module}/ansible/ --pattern star*.* -s certs --account-name dmzsandbox01 --subscription DCD-CFT-Sandbox
+			cp ~/star-platform-hmcts-net.* ${path.module}/ansible
       echo "finding Certs"
       find . -name star-platform-hmcts-net.crt
       find . -name star-platform-hmcts-net.key
