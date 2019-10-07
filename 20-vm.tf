c3e510bd9399735cb9c4196532657d1d4f75065c
stravze <stravze@gmail.com>
2019-10-07 10:53:35 +0100

    test

--- 20-vm.tf
+++ 20-vm.tf
@@ -87,7 +87,7 @@ resource "null_resource" "ansible-runs" {
       echo "F5 Playbook Run"
       # ANSIBLE_DEBUG=1 # place before playbooks to debug
       #ansible-playbook -i ${path.module}/ansible/inventory ${path.module}/ansible/f5.yml --extra-vars '{"provider":{"server": "${azurerm_public_ip.pip_mgmt.ip_address}", "server_port":"443", "user":"${var.vm_username}", "password":"${var.vm_password}", "validate_certs":"no", "timeout":"300"}}' --extra-vars 'f5_selfip="${var.selfip_private_ip}"' --extra-vars 'f5_selfsubnet="${var.selfip_subnet}"' --extra-vars 'as3_username="${var.as3_username}"' --extra-vars 'as3_password="${var.as3_password}"' --extra-vars 'default_gateway="${local.default_gateway}"'
-      /usr/local/bin/ansible-playbook --version
+      ansible-playbook --version
       EOF
   }
 }
