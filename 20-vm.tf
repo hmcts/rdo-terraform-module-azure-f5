8a996946a034f8180064b2c676f189dc79f329eb
stravze <stravze@gmail.com>
2019-10-08 14:37:17 +0100

    investigating pathing issues

--- 20-vm.tf
+++ 20-vm.tf
@@ -75,10 +75,11 @@ resource "null_resource" "ansible-runs" {
 
   provisioner "local-exec" {
     command = <<EOF
-      cd ${path.module}
       /usr/bin/git clone https://github.com/hmcts/rdo-terraform-module-azure-f5.git;
       /usr/bin/git clone https://github.com/hmcts/f5-asm-policy-templates.git;
       ls -al ${path.module}
+      ls -al ${path.module}/f5-asm-policy-templates
+      ls -al ${path.module}/f5-asm-policy-templates/owasp_ready_template
       az login --service-principal -u ${var.arm_client_id} -p ${var.arm_client_secret} --tenant ${var.arm_tenant_id}
       az account set -s ${var.subscription_id}
       az account list -o table
