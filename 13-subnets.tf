e25836f64642c1e1689ba4d4fbd85a17f1834e2c
James Johnson <2965841+jrjohnson87@users.noreply.github.com>
2019-11-25 16:25:16 +0000

    Update 13-subnets.tf

--- 13-subnets.tf
+++ 13-subnets.tf
@@ -1,5 +1,5 @@
 resource "azurerm_subnet" "f5_data_subnet" {
-  name                        = "${var.env_name}-mgmt"
+  name                        = "${var.env_name}-data"
   resource_group_name         = "${azurerm_resource_group.f5-rg.name}"
   virtual_network_name        = "${azurerm_virtual_network.f5_vnet.name}"
   address_prefix              = "${var.loadbalancer_data_subnet_prefix}"
